dda01915c13475990a4440a98928e4e9f7942636
Vicente <busslina@gmail.com>
2024-05-19 19:44:29 +0200

    snapshot

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rearch/flutter_rearch.dart';
+import 'package:rearch/rearch.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const RearchBootstrapper(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -11,9 +13,31 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: KeyTestWidget(),
+      ),
+    );
+  }
+}
+
+class KeyTestWidget extends RearchConsumer {
+  const KeyTestWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetHandle use) {
+    final focusNode = use.focusNode(
+      onKeyEvent: (node, event) {
+        print('onKeyEvent -- ${event.logicalKey}');
+
+        return KeyEventResult.ignored;
+      },
+    );
+
+    use.callonce(() => Future.microtask(focusNode.requestFocus));
+
+    return Focus(
+      focusNode: focusNode,
+      child: Container(
+        color: Colors.yellow.withOpacity(0.6),
       ),
     );
   }
